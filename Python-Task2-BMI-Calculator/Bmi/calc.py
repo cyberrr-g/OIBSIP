7429ea47ee7764871520a46f6e14d00f74144c24
cyberrr-g <cyberrr_03@proton.me>
2026-09-03 20:36:50 +0530

    main file and calc.

---
+++ Python-Task2-BMI-Calculator/Bmi/calc.py
@@ -0,0 +1,17 @@
+
+
+
+def calc_bmi(weight, height_cm):
+    height_m = height_cm / 100.0
+    return weight / (height_m ** 2)
+
+
+def classify_bmi(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif bmi < 25:
+        return "Normal"
+    elif bmi < 30:
+        return "Overweight"
+    else:
+        return "Obese"
